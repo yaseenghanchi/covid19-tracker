863b8edffdc7ee849fa15b3fe410f8f299a6cd1e
Yaseen Ghanchi <8342511+yaseenghanchi@users.noreply.github.com>
2020-07-08 19:01:49 +0500

    some changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 import Header from './components/header';
+import Cards from './components/content/cards/cards'
+
+
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  }
+}));
+
 function App() {
+
+
+  const classes = useStyles();
+
+
+
+
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
+    <>
+    <Header/>
+    <Cards />
+    {/* <div className={classes.root}>
+    <Grid container spacing={3}>
+      <Grid item xs>
+
+      </Grid>
+      <Grid item xs>
+      </Grid>
+      <Grid item xs>
+      </Grid>
+    
+    </Grid>
+    </div> */}
+    </>
   );
 }
 
---
+++ src/components/content/cards/cards.js
@@ -0,0 +1,28 @@
+import React, {useState, useEffect} from 'react';
+
+const Cards = () => {
+    const apiUrl =  "https://covid19.mathdro.id/api";
+
+    const [globalData, setGlobalData] = useState({});
+
+    useEffect(() => {
+        const fetchApiData = async () => {
+            const data = await fetch(apiUrl)
+            const resp = await data.json();
+           setGlobalData(resp);
+           console.log(resp);
+           console.log(globalData);
+        }
+        fetchApiData();
+    }, []);
+
+    
+
+    return (
+        <div>
+            Cards
+        </div>
+    );
+};
+
+export default Cards;
